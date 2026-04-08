4169c59ecaa6168cfe7470bffe127e5b48deafff
Tri Le <zeroeffects@hotmail.com>
2026-04-08 15:52:01 -0400

    Revert "Completed login orm"
    This reverts commit dbab042c4d08145cde611b432657dc4917c67747.

--- MusicLibrary/Form1.cs
+++ MusicLibrary/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MusicLibrary
 {
-    public partial class frmLogin : Form
+    public partial class Form1 : Form
     {
-        public frmLogin()
+        public Form1()
         {
             InitializeComponent();
         }
--- MusicLibrary/Program.cs
+++ MusicLibrary/Program.cs
@@ -16,7 +16,7 @@ namespace MusicLibrary
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new Form1());
         }
     }
 }

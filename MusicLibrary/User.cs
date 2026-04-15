b30df7819143f2e46916e50ad40613e53b0972d2
Tri Le <zeroeffects@hotmail.com>
2026-04-15 16:00:48 -0400

    Added and connected initial database file to project. Created User constructor.

---
+++ MusicLibrary/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public String UserName { get; set; }
+        public String UserPassword { get; set; }
+    }
+}

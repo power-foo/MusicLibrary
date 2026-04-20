11d36c5fca8f9e98ad66a83fddfbf29bf6c06c9f
dchrisiu <150213741+dchrisiu@users.noreply.github.com>
2026-04-19 23:12:31 -0400

    Added a loading playlist and then adding a playlist

--- MusicLibrary/FrmViewPlaylist.Designer.cs
+++ MusicLibrary/FrmViewPlaylist.Designer.cs
@@ -28,44 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dgvPlaylists = new System.Windows.Forms.DataGridView();
+            this.btnLoadPlaylists = new System.Windows.Forms.Button();
+            this.txtPlaylistName = new System.Windows.Forms.TextBox();
+            this.btnAddPlaylist = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlaylists)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // dgvPlaylists
             // 
-            this.button1.Location = new System.Drawing.Point(219, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.dgvPlaylists.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlaylists.Location = new System.Drawing.Point(34, 31);
+            this.dgvPlaylists.Name = "dgvPlaylists";
+            this.dgvPlaylists.Size = new System.Drawing.Size(381, 150);
+            this.dgvPlaylists.TabIndex = 0;
             // 
-            // button2
+            // btnLoadPlaylists
             // 
-            this.button2.Location = new System.Drawing.Point(219, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnLoadPlaylists.Location = new System.Drawing.Point(34, 201);
+            this.btnLoadPlaylists.Name = "btnLoadPlaylists";
+            this.btnLoadPlaylists.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPlaylists.TabIndex = 1;
+            this.btnLoadPlaylists.Text = "Load Playlists";
+            this.btnLoadPlaylists.UseVisualStyleBackColor = true;
+            this.btnLoadPlaylists.Click += new System.EventHandler(this.btnLoadPlaylists_Click);
+            // 
+            // txtPlaylistName
+            // 
+            this.txtPlaylistName.Location = new System.Drawing.Point(34, 250);
+            this.txtPlaylistName.Name = "txtPlaylistName";
+            this.txtPlaylistName.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaylistName.TabIndex = 2;
+            // 
+            // btnAddPlaylist
+            // 
+            this.btnAddPlaylist.Location = new System.Drawing.Point(34, 292);
+            this.btnAddPlaylist.Name = "btnAddPlaylist";
+            this.btnAddPlaylist.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPlaylist.TabIndex = 3;
+            this.btnAddPlaylist.Text = "Add Playlist";
+            this.btnAddPlaylist.UseVisualStyleBackColor = true;
+            this.btnAddPlaylist.Click += new System.EventHandler(this.btnAddPlaylist_Click);
             // 
             // FrmViewPlaylist
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddPlaylist);
+            this.Controls.Add(this.txtPlaylistName);
+            this.Controls.Add(this.btnLoadPlaylists);
+            this.Controls.Add(this.dgvPlaylists);
             this.Name = "FrmViewPlaylist";
             this.Text = "FrmViewPlaylist";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlaylists)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvPlaylists;
+        private System.Windows.Forms.Button btnLoadPlaylists;
+        private System.Windows.Forms.TextBox txtPlaylistName;
+        private System.Windows.Forms.Button btnAddPlaylist;
     }
 }
--- MusicLibrary/FrmViewPlaylist.cs
+++ MusicLibrary/FrmViewPlaylist.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace MusicLibrary
 {
@@ -15,6 +16,74 @@ namespace MusicLibrary
         public FrmViewPlaylist()
         {
             InitializeComponent();
+        }
+
+        private void btnLoadPlaylists_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            OleDbConnection myConnection;
+            OleDbCommand myCommand;
+            OleDbDataAdapter myAdapter;
+            DataTable myTable;
+            string sql;
+
+            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\MusicLibrary.accdb";
+            myConnection = new OleDbConnection(connectionString);
+
+            try
+            {
+                myConnection.Open();
+
+                sql = "SELECT * FROM Playlist";
+                myCommand = new OleDbCommand(sql, myConnection);
+
+                myAdapter = new OleDbDataAdapter(myCommand);
+                myTable = new DataTable();
+
+                myAdapter.Fill(myTable);
+
+                dgvPlaylists.DataSource = myTable;
+
+                myConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading playlists: " + ex.Message);
+            }
+        }
+
+        private void btnAddPlaylist_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            OleDbConnection myConnection;
+            OleDbCommand myCommand;
+            string sql;
+
+            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\MusicLibrary.accdb";
+            myConnection = new OleDbConnection(connectionString);
+
+            try
+            {
+                myConnection.Open();
+
+                sql = "INSERT INTO Playlist (PlaylistName, UserID) VALUES (?, ?)";
+                myCommand = new OleDbCommand(sql, myConnection);
+
+                myCommand.Parameters.AddWithValue("?", txtPlaylistName.Text);
+                myCommand.Parameters.AddWithValue("?", 1);
+
+                myCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Playlist added successfully.");
+
+                myConnection.Close();
+
+                btnLoadPlaylists_Click(sender, e);
+            }
+            catch
+            {
+                MessageBox.Show("Can not add a playlist");
+            }
         }
     }
 }

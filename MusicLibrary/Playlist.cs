57634d8c96613c9954d87a6139c860730d2b74a3
dchrisiu <150213741+dchrisiu@users.noreply.github.com>
2026-04-24 23:19:49 -0400

    .

---
+++ MusicLibrary/Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary
+{
+    public class Playlist
+    {
+        public int PlaylistID { get; set; }
+        public string PlaylistName { get; set; }
+        public int UserID { get; set; }
+    }
+}
